d367e31af8dfa4d3631d6e23d05e24fa0af00508
Rai Tamarindo <raitamarindo@gmail.com>
2019-07-03 15:48:47 -0300

    Pipeline process

--- main.go
+++ main.go
@@ -1,9 +1,68 @@
-package pxcbenchmark
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
+
+func worker(job []int, dev, mean float64) []int {
+	timeout := time.Duration(math.Round(rand.NormFloat64()*dev + mean))
+	for i := range job {
+		job[i]++
+	}
+	time.Sleep(timeout * time.Millisecond)
+
+	return job
+}
+
+func pipelineWay(count, size int, dev, mean float64) {
+	stage1 := make(chan []int)
+	stage2 := make(chan []int)
+	stage3 := make(chan []int)
+	stage4 := make(chan []int)
+
+	fmt.Println("starting pipeline")
+
+	go func() {
+		for i := 0; i < count; i++ {
+			job := make([]int, size)
+			stage1 <- worker(job[:], dev, mean)
+			fmt.Printf("stage #%d processed job size %d\n", job[0], len(job))
+		}
+		close(stage1)
+	}()
+
+	go func() {
+		for job := range stage1 {
+			stage2 <- worker(job, dev, mean)
+			fmt.Printf("stage #%d processed job size %d\n", job[0], len(job))
+		}
+		close(stage2)
+	}()
+
+	go func() {
+		for job := range stage2 {
+			stage3 <- worker(job, dev, mean)
+			fmt.Printf("stage #%d processed job size %d\n", job[0], len(job))
+		}
+		close(stage3)
+	}()
+
+	go func() {
+		for job := range stage3 {
+			worker(job, dev, mean)
+			fmt.Printf("stage #%d processed job size %d\n", job[0], len(job))
+		}
+		close(stage4)
+	}()
+
+	<-stage4
+}
 
 func main() {
-	
-	fmt.Println("ola")
-	
+
+	pipelineWay(10, 1000, 10, 100)
+
 }
