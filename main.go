bf40873b36dc840958f882e3fe1abcfeca0e900e
Rai Tamarindo <raitamarindo@gmail.com>
2019-07-03 16:12:41 -0300

    Concurrent process

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func pipelineWay(count, size int, dev, mean float64) {
 	go func() {
 		for i := 0; i < count; i++ {
 			job := make([]int, size)
-			stage1 <- worker(job[:], dev, mean)
+			stage1 <- worker(job, dev, mean)
 		}
 		close(stage1)
 	}()
@@ -54,8 +54,72 @@ func pipelineWay(count, size int, dev, mean float64) {
 	<-stage4
 }
 
+func concurrentWay(count, size int, dev, mean float64) {
+	stage1 := make(chan []int)
+	stage2 := make(chan []int)
+	stage3 := make(chan []int)
+	stage4 := make(chan []int)
+
+	go func() {
+		for i := 0; i < count; i++ {
+			if i%4 == 0 {
+				j0 := make([]int, size)
+				j1 := worker(j0, dev, mean)
+				j2 := worker(j1, dev, mean)
+				j3 := worker(j2, dev, mean)
+				worker(j3, dev, mean)
+			}
+		}
+		close(stage1)
+	}()
+
+	go func() {
+		for i := 0; i < count; i++ {
+			if i%4 == 1 {
+				j0 := make([]int, size)
+				j1 := worker(j0, dev, mean)
+				j2 := worker(j1, dev, mean)
+				j3 := worker(j2, dev, mean)
+				worker(j3, dev, mean)
+			}
+		}
+		close(stage2)
+	}()
+
+	go func() {
+		for i := 0; i < count; i++ {
+			if i%4 == 2 {
+				j0 := make([]int, size)
+				j1 := worker(j0, dev, mean)
+				j2 := worker(j1, dev, mean)
+				j3 := worker(j2, dev, mean)
+				worker(j3, dev, mean)
+			}
+		}
+		close(stage3)
+	}()
+
+	go func() {
+		for i := 0; i < count; i++ {
+			if i%4 == 3 {
+				j0 := make([]int, size)
+				j1 := worker(j0, dev, mean)
+				j2 := worker(j1, dev, mean)
+				j3 := worker(j2, dev, mean)
+				worker(j3, dev, mean)
+			}
+		}
+		close(stage4)
+	}()
+
+	<-stage1
+	<-stage2
+	<-stage3
+	<-stage4
+}
+
 func main() {
 
-	pipelineWay(10, 1000, 10, 100)
+	concurrentWay(10, 1000, 10, 100)
 
 }
